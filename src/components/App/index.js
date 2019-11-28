1c398acfb3ea97ddb0e07546bd941c2fd5c2b7ae
Safdar Jamal <safdar.dev@gmail.com>
2019-11-28 21:12:41 +0500

    Create app component

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const App = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+};
+
+export default App;
