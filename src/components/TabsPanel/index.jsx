d652fd58560e1665ab610e671bd3398b64ca6703
Safdar Jamal <safdar.dev@gmail.com>
2019-12-02 21:55:55 +0500

    Move tab state from TabsPanel to Main component

--- src/components/TabsPanel/index.jsx
+++ src/components/TabsPanel/index.jsx
@@ -1,37 +1,24 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1
-  }
-});
-
-const TabsPanel = () => {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
+const TabsPanel = ({ tabStatus, setTabStatus }) => {
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    setTabStatus(newValue);
   };
 
   return (
-    <Paper className={classes.root}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        centered
-        variant="fullWidth"
-      >
-        <Tab label="Simple Interest Calculator" />
-        <Tab label="Compound Interest Calculator" />
-      </Tabs>
-    </Paper>
+    <Tabs
+      value={tabStatus}
+      onChange={handleChange}
+      indicatorColor="primary"
+      textColor="primary"
+      centered
+      variant="fullWidth"
+    >
+      <Tab label="Simple Interest Calculator" />
+      <Tab label="Compound Interest Calculator" />
+    </Tabs>
   );
 };
 
