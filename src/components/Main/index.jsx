e72351b8e721de8ad2607f60f7a425d9a131ac38
Safdar Jamal <safdar.dev@gmail.com>
2019-12-02 21:54:56 +0500

    Create main component to wrap tabs and forms

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import TabsPanel from '../TabsPanel';
+import SimpleInterest from '../SimpleInterest';
+import CompoundInterest from '../CompoundInterest';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    marginTop: 50,
+    marginBottom: 50
+  }
+});
+
+const Main = () => {
+  const classes = useStyles();
+  const [tabStatus, setTabStatus] = useState(0);
+
+  return (
+    <Container>
+      <Paper elevation={2} className={classes.root}>
+        <TabsPanel tabStatus={tabStatus} setTabStatus={setTabStatus} />
+        {tabStatus === 0 && <SimpleInterest />}
+        {tabStatus === 1 && <CompoundInterest />}
+      </Paper>
+    </Container>
+  );
+};
+
+export default Main;
