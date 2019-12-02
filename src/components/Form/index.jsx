01a46f43c4fd7051ddf7469270bf3057eee58caf
Safdar Jamal <safdar.dev@gmail.com>
2019-12-02 20:29:17 +0500

    Create form state

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,35 +1,107 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   root: {
     '& > *': {
       margin: theme.spacing(2),
       width: 300
     }
   }
-}));
+});
 
-const Form = () => {
-  const classes = useStyles();
+class Form extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Paper elevation={2}>
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </form>
-    </Paper>
-  );
-};
+    this.state = {
+      initialInvestment: '',
+      interestRate: '',
+      calculationPeriod: '',
+      compoundInterval: '',
+      regularInvestment: ''
+    };
+  }
 
-export default Form;
+  handleChange = e => {
+    this.setState({ [e.target.name]: Number(e.target.value) });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    const {
+      initialInvestment,
+      interestRate,
+      calculationPeriod,
+      compoundInterval,
+      regularInvestment
+    } = this.state;
+    console.log(this.state);
+
+    const { classes } = this.props;
+
+    return (
+      <Paper elevation={2}>
+        <form
+          onSubmit={this.handleSubmit}
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            name="initialInvestment"
+            label="Initial Investment"
+            variant="outlined"
+            type="number"
+            value={initialInvestment}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="interestRate"
+            label="Interest Rate (Yearly)"
+            variant="outlined"
+            type="number"
+            value={interestRate}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="calculationPeriod"
+            label="Calculation Period"
+            variant="outlined"
+            type="number"
+            value={calculationPeriod}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="compoundInterval"
+            label="Compound Interval"
+            variant="outlined"
+            type="number"
+            value={compoundInterval}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="regularInvestment"
+            label="Regular Investment"
+            variant="outlined"
+            type="number"
+            value={regularInvestment}
+            onChange={this.handleChange}
+          />
+          <br />
+          <Button type="submit" variant="contained" color="primary">
+            Calculate
+          </Button>
+        </form>
+      </Paper>
+    );
+  }
+}
+
+export default withStyles(styles)(Form);
