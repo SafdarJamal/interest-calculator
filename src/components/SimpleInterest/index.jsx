fe07a218b54e2b7b32185007e43295b62b9230c4
Safdar Jamal <safdar.dev@gmail.com>
2019-12-10 22:32:35 +0500

    Integrate result in simple-interest component

--- src/components/SimpleInterest/index.jsx
+++ src/components/SimpleInterest/index.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import { calculateSimpleInterest } from '../../utils/calculateSimpleInterest';
+import Result from '../Result';
 
 const styles = theme => ({
   root: {
@@ -39,7 +40,12 @@ class SimpleInterest extends Component {
       initialInvestment: '',
       interestRate: '',
       calculationPeriod: '',
-      calculationPeriodType: 1
+      calculationPeriodType: 1,
+      resultData: [
+        { name: 'Initial Investment', value: 0 },
+        { name: 'Interest Earned', value: 0 },
+        { name: 'Total', value: 0 }
+      ]
     };
   }
 
@@ -53,13 +59,24 @@ class SimpleInterest extends Component {
     const {
       initialInvestment,
       interestRate,
-      calculationPeriodType
+      calculationPeriodType,
+      resultData
     } = this.state;
 
     let { calculationPeriod } = this.state;
     calculationPeriod = calculationPeriod / calculationPeriodType / 1;
 
-    calculateSimpleInterest(initialInvestment, interestRate, calculationPeriod);
+    const { P, I, A } = calculateSimpleInterest(
+      initialInvestment,
+      interestRate,
+      calculationPeriod
+    );
+
+    resultData[0].value = P;
+    resultData[1].value = I;
+    resultData[2].value = A;
+
+    this.setState({ resultData });
   };
 
   handleReset = e => {
@@ -67,7 +84,12 @@ class SimpleInterest extends Component {
       initialInvestment: '',
       interestRate: '',
       calculationPeriod: '',
-      calculationPeriodType: 1
+      calculationPeriodType: 1,
+      resultData: [
+        { name: 'Initial Investment', value: 0 },
+        { name: 'Interest Earned', value: 0 },
+        { name: 'Total', value: 0 }
+      ]
     });
   };
 
@@ -76,7 +98,8 @@ class SimpleInterest extends Component {
       initialInvestment,
       interestRate,
       calculationPeriod,
-      calculationPeriodType
+      calculationPeriodType,
+      resultData
     } = this.state;
 
     const { classes } = this.props;
@@ -87,86 +110,89 @@ class SimpleInterest extends Component {
     }
 
     return (
-      <form
-        className={classes.root}
-        noValidate
-        autoComplete="off"
-        onSubmit={this.handleSubmit}
-        onReset={this.handleReset}
-      >
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField
-              className={classes.input}
-              autoFocus
-              name="initialInvestment"
-              label="Initial Investment"
-              variant="outlined"
-              type="number"
-              value={initialInvestment}
-              onChange={this.handleChange}
-            />
+      <Fragment>
+        <form
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+          onSubmit={this.handleSubmit}
+          onReset={this.handleReset}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                className={classes.input}
+                autoFocus
+                name="initialInvestment"
+                label="Initial Investment"
+                variant="outlined"
+                type="number"
+                value={initialInvestment}
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                className={classes.input}
+                name="interestRate"
+                label="Yearly Interest Rate (%)"
+                variant="outlined"
+                type="number"
+                value={interestRate}
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                className={classes.input}
+                name="calculationPeriod"
+                label="Calculation Period"
+                variant="outlined"
+                type="number"
+                value={calculationPeriod}
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                className={classes.input}
+                name="calculationPeriodType"
+                variant="outlined"
+                select
+                value={calculationPeriodType}
+                onChange={this.handleChange}
+              >
+                <MenuItem value={365}>Days</MenuItem>
+                <MenuItem value={12}>Months</MenuItem>
+                <MenuItem value={1}>Years</MenuItem>
+              </TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                className={classes.calcButton}
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="large"
+                disabled={!isFormFilled}
+              >
+                Calculate
+              </Button>
+              <Button
+                className={classes.resetButton}
+                type="reset"
+                variant="contained"
+                color="secondary"
+                size="large"
+                disabled={!isFormFilled}
+              >
+                Reset
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              className={classes.input}
-              name="interestRate"
-              label="Yearly Interest Rate (%)"
-              variant="outlined"
-              type="number"
-              value={interestRate}
-              onChange={this.handleChange}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              className={classes.input}
-              name="calculationPeriod"
-              label="Calculation Period"
-              variant="outlined"
-              type="number"
-              value={calculationPeriod}
-              onChange={this.handleChange}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              className={classes.input}
-              name="calculationPeriodType"
-              variant="outlined"
-              select
-              value={calculationPeriodType}
-              onChange={this.handleChange}
-            >
-              <MenuItem value={365}>Days</MenuItem>
-              <MenuItem value={12}>Months</MenuItem>
-              <MenuItem value={1}>Years</MenuItem>
-            </TextField>
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              className={classes.calcButton}
-              type="submit"
-              variant="contained"
-              color="primary"
-              size="large"
-              disabled={!isFormFilled}
-            >
-              Calculate
-            </Button>
-            <Button
-              className={classes.resetButton}
-              type="reset"
-              variant="contained"
-              color="secondary"
-              size="large"
-              disabled={!isFormFilled}
-            >
-              Reset
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+        </form>
+        <Result resultData={resultData} />
+      </Fragment>
     );
   }
 }
