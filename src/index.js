e8b5a1ab4a712897b28da85379ed80b317ce403d
Safdar Jamal <safdar.dev@gmail.com>
2020-10-03 19:52:00 +0500

    Update boilerplate

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 serviceWorker.register();
